6fa86ee027c6570f8344fcbad93a7d125509064f
ManojUchiha14 <b.manoj9859@gmail.com>
2022-06-17 18:19:46 +0530

    first commit

---
+++ app3/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def date_14_06(request):
+    return HttpResponse('<h2><i><marquee>baseurl</marquee></i></h2>')
