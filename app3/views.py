1ab727de43a32a1b98b2c8e6c3aa5d7f17a882ab
ManojUchiha14 <107688798+ManojUchiha14@users.noreply.github.com>
2022-06-18 18:15:32 +0530

    Update views.py

--- app3/views.py
+++ app3/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def date_14_06(request):
-    return HttpResponse('<h2><i><marquee>baseurl</marquee></i></h2>')
+    return HttpResponse('<h2><i><marquee>baseurl</marquee></i></h2><br><img src="1111994.jpg" alt="" width="500px" height="500px">')
